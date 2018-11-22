beaec0647e7790a454d46198643c56e107cd215d
fke <362508572@qq.com>
2018-11-22 11:12:48 +0800

    修复bug

--- app/api_1_0/file_manage.py
+++ app/api_1_0/file_manage.py
@@ -9,6 +9,8 @@ def api_upload():
     data = request.files
     # try:
     file = data['file']
+    if os.path.exists(os.path.join(FILE_ADDRESS, file.filename)):
+        return jsonify({"msg": "文件已存在，请修改文件名字后再上传", "status": 0})
     file.save(os.path.join(FILE_ADDRESS, file.filename))
 
     return jsonify({'data': os.path.join(FILE_ADDRESS, file.filename), "msg": "上传成功", "status": 1})
