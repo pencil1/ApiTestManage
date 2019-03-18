61e147b4d2d76a5c9ff552a31ecbba3e308e197b
fke <362508572@qq.com>
2019-03-18 17:00:28 +0800

    优化代码

--- app/util/custom_decorator.py
+++ app/util/custom_decorator.py
@@ -11,7 +11,6 @@ def login_required(func):
         elif not current_user.is_authenticated:
             return jsonify({'msg': '登录超时,请重新登录', 'status': 0})
         return func(*args, **kwargs)
-
     return decorated_view
 
 
@@ -23,7 +22,9 @@ def permission_required(permission_name):
             if not current_user.can(permission_name):
                 return jsonify({'msg': '没有该权限', 'status': 0})
             return func(*args, **kwargs)
+
         return decorated_function
+
     return decorator
 
 
